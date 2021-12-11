7be1badbecd386f07fef42716e69ed35aca6c9e2
Xan0er <singhhh890.gs@gmail.com>
2021-12-11 11:48:25 +0530

    updated test.js

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
-console.log("My script is working")
-alert("My script is working")
+// console.log("My script is working")
+// alert("My script is working")
+
+// Fetching Cookies
+function getCookie(c_name) {
+    let x = document.cookie;
+    return x
+}
+
+console.log("tracking_id", getCookie("tracking_id"))
